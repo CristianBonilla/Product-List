626ca4807d6063d836255d945026f4d0ce1d0a90
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2019-06-23 17:35:47 -0500

    Ajuste del evento de transición toggle navbar

--- src/app/views/components/navbar/navbar.component.ts
+++ src/app/views/components/navbar/navbar.component.ts
@@ -1,69 +1,21 @@
-import { Component, OnInit, ViewChild, ElementRef, Inject, AfterViewInit } from '@angular/core';
-import { WINDOW } from '@core/window.provider';
-import { EVENT_END, EventEnd, TransitionEvent } from '@core/event-end';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'pl-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit, AfterViewInit {
-  private readonly document: Document;
-  private readonly html: HTMLElement;
-  private readonly body: HTMLElement;
-  private readonly setTimeout: (handler: TimerHandler, wait: number) => void;
-  private readonly transitionEnd: TransitionEvent;
-  private navbarMenuVisible = false;
-
   @ViewChild('toggle', { static: false })
   private readonly toggleElementRef: ElementRef<HTMLElement>;
 
-  constructor(
-    @Inject(WINDOW) { document, setTimeout }: Window,
-    @Inject(EVENT_END) { transitionEnd }: EventEnd) {
-    this.document = document;
-    this.html = document.documentElement;
-    this.body = document.body;
-    this.setTimeout = setTimeout;
-    this.transitionEnd = transitionEnd;
-  }
+  toggleElement: HTMLElement;
+
+  constructor() { }
 
   ngOnInit() { }
 
   ngAfterViewInit() {
     const { nativeElement } = this.toggleElementRef;
-    nativeElement.addEventListener('click', () => {
-      if (this.navbarMenuVisible) {
-        this.removeBodyClickElement(nativeElement);
-      } else {
-        this.setTimeout(() => {
-          nativeElement.classList.add('toggled');
-        }, 580);
-        const bodyClickElement = this.document.createElement('div');
-        bodyClickElement.id = 'bodyClick';
-        this.body.append(bodyClickElement);
-        const bodyClick = this.getBodyClickElement();
-        bodyClick.addEventListener('click', () => {
-          this.removeBodyClickElement(nativeElement);
-        }, false);
-      }
-      this.html.classList.add('nav-open');
-      this.navbarMenuVisible = true;
-    }, false);
-  }
-
-  private removeBodyClickElement(toggleElement: HTMLElement) {
-    this.html.classList.remove('nav-open');
-    this.navbarMenuVisible = false;
-    this.setTimeout(() => {
-      toggleElement.classList.remove('toggled');
-      const bodyClickElement = this.getBodyClickElement();
-      if (bodyClickElement) {
-        bodyClickElement.remove();
-      }
-    }, 550);
-  }
-
-  private getBodyClickElement(): Element {
-    return this.body.querySelector('#bodyClick');
+    this.toggleElement = nativeElement;
   }
 }
--- src/app/views/components/sidebar/sidebar.component.ts
+++ src/app/views/components/sidebar/sidebar.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'pl-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
+  @ViewChild('sidebar', { static: false })
+  private readonly sidebarElementRef: ElementRef<HTMLElement>;
+
+  sidebarElement: HTMLElement;
+
   constructor() { }
 
   ngOnInit() { }
+
+  ngAfterViewInit() {
+    const { nativeElement } = this.sidebarElementRef;
+    this.sidebarElement = nativeElement;
+  }
 }
