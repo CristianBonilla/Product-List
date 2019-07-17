38f72409a93cbd6b44cd7e39f9752b8fa93a676f
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2019-07-16 21:22:39 -0500

    Ajuste de estilos del scroll OverlayScrollbars

--- src/app/views/components/sidebar/sidebar.component.ts
+++ src/app/views/components/sidebar/sidebar.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Inject } from '@angular/core';
+import { WINDOW } from '@core/window.provider';
 
 @Component({
   selector: 'pl-sidebar',
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent implements OnInit, AfterViewInit {
+  private readonly body: HTMLElement;
+
   @ViewChild('sidebar', { static: false })
   private readonly sidebarElementRef: ElementRef<HTMLElement>;
 
   sidebarElement: HTMLElement;
 
-  constructor() { }
+  constructor(@Inject(WINDOW) { document }: Window) {
+    this.body = document.body;
+  }
 
   ngOnInit() { }
 
   ngAfterViewInit() {
     const { nativeElement } = this.sidebarElementRef;
     this.sidebarElement = nativeElement;
+  }
+
+  minimize() {
+    this.body.classList.toggle('sidebar-mini');
   }
 }
